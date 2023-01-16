b0419daf6fecfe1b60f0e0ea54d95c21ffb1901a
VincentPeer <vincent.peer@heig-vd.ch>
2023-01-16 15:11:12 +0100

    project init

---
+++ app/src/main/java/ch/heigvd/daa/keystore/MainActivity.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.daa.keystore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
