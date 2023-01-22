43d5890c2b653890c114744d16d23b5243e68c8f
VincentPeer <vincent.peer@heig-vd.ch>
2023-01-22 15:26:49 +0100

    encrypt seems ok

--- app/src/main/java/ch/heigvd/daa/keystore/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa/keystore/MainActivity.kt
@@ -9,64 +9,49 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import ch.heigvd.daa.keystore.contract.PickNameContract
+import java.io.File
+import java.io.FileOutputStream
+import ch.heigvd.daa.keystore.EncryptionUtils
+import org.w3c.dom.Text
+import java.io.FileInputStream
 
-private const val SAVED_STATE_NAME_KEY = "NAME"
 
 /**
  * Displays a personalized welcome message and allows the user to edit his name.
  */
 class MainActivity : AppCompatActivity() {
+    private val encryptionUtils = EncryptionUtils()
+    private var messageToEncrypt = "message to encrypt"
+    private var messageToDecrypt: String? = null
 
-    private var name: String? = null
 
-    // Collect the username through the contract with the EditName class
-    private val manageUsername = registerForActivityResult(PickNameContract()) {
-        if (it != null) {
-            name = it
-            updateWelcomeText()
-        }
-
-    }
-
-    /**
-     * Displays a layout with
-     * - A welcome text based on the user name
-     * - A button that allows the user to edit his name
-     * If a previous activity state was saved, the user name is restored from it.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        name = savedInstanceState?.getString(SAVED_STATE_NAME_KEY)
-
         setContentView(R.layout.activity_main)
+        val message = findViewById<TextView>(R.id.msg_to_encrypt)
+        message.text = messageToEncrypt
 
-        updateWelcomeText()
+        findViewById<Button>(R.id.encrypt_button).setOnClickListener {
+            val bytes = messageToEncrypt.encodeToByteArray()
+            val file = File(filesDir, "secret.txt")
+            if(!file.exists()) {
+                file.createNewFile()
+            }
+            val fos = FileOutputStream(file)
 
-        // When the button is pressed, we retrieve the user name using the contract linked to manageUsername
-        findViewById<Button>(R.id.edit_button).setOnClickListener {
-            manageUsername.launch(name)
+            messageToDecrypt = encryptionUtils.encrypt(bytes, fos).decodeToString()
+            findViewById<TextView>(R.id.encrypted_msg).text = messageToEncrypt
         }
-    }
 
-    /**
-     * Updates the welcome text displayed according to the user name.
-     * If the name is null, a generic welcome text is displayed.
-     * Otherwise, a personalized welcome text is displayed.
-     */
-    private fun updateWelcomeText() {
-        findViewById<TextView>(R.id.welcome).text =
-            if (name == null) getString(R.string.ask_for_name)
-            else getString(R.string.welcome_user, name)
-    }
+        findViewById<Button>(R.id.decrypt_button).setOnClickListener {
+            val file = File(filesDir, "secret.txt")
+            messageToEncrypt = encryptionUtils.decrypt(
+                inputStream = FileInputStream(file)
+            ).decodeToString()
 
-    /**
-     * Saves the user name
-     */
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(SAVED_STATE_NAME_KEY, name)
-    }
+            findViewById<TextView>(R.id.decrypted_msg).text = messageToEncrypt
 
+        }
+
+    }
 }
