3e43660f6e2f00ba1d2c21ebc976ccdd3fee87f4
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2023-01-25 14:44:00 +0100

    fix decrypt crash UI

--- app/src/main/java/ch/heigvd/daa/keystore/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa/keystore/MainActivity.kt
@@ -5,39 +5,53 @@
 
 package ch.heigvd.daa.keystore
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Base64
 
 
 /**
  * Displays a personalized welcome message and allows the user to edit his name.
  */
 class MainActivity : AppCompatActivity() {
-    private val keystore = EncryptorSym("secret")
-    private lateinit var encryptedMsg : EncryptedFileBox
-    private lateinit var decryptedMsg : ByteArray
-
-
+    private val encryptor = EncryptorSym("secret")
+    private lateinit var encryptedMsg: EncryptedFileBox
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val textToEncrypt = findViewById<EditText>(R.id.msg_to_encrypt)
+        val cipherTextView = findViewById<TextView>(R.id.encrypted_msg)
+        val cipherTextIvView = findViewById<TextView>(R.id.encrypted_msg_iv)
 
         findViewById<Button>(R.id.encrypt_button).setOnClickListener {
-            encryptedMsg = keystore.encrypt(textToEncrypt.text.toString().encodeToByteArray())
-            findViewById<TextView>(R.id.encrypted_msg).text =
-                "Encrypted message : ${encryptedMsg.cipherText}"
+            encryptedMsg = encryptor.encrypt(textToEncrypt.text.toString().encodeToByteArray())
+            cipherTextView.text = Base64.getEncoder().encodeToString(encryptedMsg.cipherText)
+            cipherTextIvView.text = Base64.getEncoder().encodeToString(encryptedMsg.iv)
         }
 
         findViewById<Button>(R.id.decrypt_button).setOnClickListener {
-            decryptedMsg = keystore.decrypt(
-                encryptedMsg
-            )
-            findViewById<TextView>(R.id.decrypted_msg).text =
-                "Decrypted message : " + decryptedMsg.decodeToString()
+            val cipherText: String = cipherTextView.text.toString()
+            val iv = cipherTextIvView.text.toString()
+            if (iv.isNotEmpty() && cipherText.isNotEmpty()) {
+                val cipherTextDec = Base64.getDecoder().decode(cipherText)
+                val ivDec = Base64.getDecoder().decode(iv)
+                try {
+                    val decryptedMsg = encryptor.decrypt(
+                        EncryptedFileBox(cipherText = cipherTextDec, iv = ivDec)
+                    )
+                    findViewById<TextView>(R.id.decrypted_msg).text = decryptedMsg.decodeToString()
+                } catch (e: java.lang.Exception) {
+                    Toast.makeText(this, e.message, LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
